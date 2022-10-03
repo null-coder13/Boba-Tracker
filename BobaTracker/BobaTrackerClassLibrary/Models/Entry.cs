add1463dc9a529889f1198d4e88b506936f88cbc
Tom <trgadacy@quinnipiac.edu>
2022-10-03 15:05:48 -0400

    Setup basic project structure

---
+++ BobaTracker/BobaTrackerClassLibrary/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BobaTrackerClassLibrary.Models
+{
+    public class Entry
+    {
+        public DateTime DateTimeId { get; set; }
+        public bool hasPooped { get; set; }
+        public bool hasPeed { get; set; }
+
+    }
+}
